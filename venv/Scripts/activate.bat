20f94cef0d57acdf52f6ab266dcedde28aef3102
Dewi Safira <dewisafira@student.gunadarma.ac.id>
2025-06-28 16:58:11 +0700

    updated requirements

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\OneDrive - student.gunadarma.ac.id\Documents\GUNADARMA\SEMESTER 6\PENELITIAN ILMIAH OMG\beneran\dashboard\venv
+set VIRTUAL_ENV=D:\OneDrive - student.gunadarma.ac.id\Documents\GUNADARMA\SEMESTER 6\PENELITIAN ILMIAH OMG\beneran\dashboard\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
